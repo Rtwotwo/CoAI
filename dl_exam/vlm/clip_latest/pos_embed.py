d7bda92d2b52071aa8b2c16ca8c18d8b7354834f
Rtwotwo <yoloredal@gmail.com>
2025-12-03 22:15:03 +0800

    Update GELU

---
+++ dl_exam/vlm/clip_latest/pos_embed.py
@@ -0,0 +1,6 @@
+"""
+Author: Redal
+Date: 2025-12-03
+Todo: 
+Homepage: https://github.com/Rtwotwo/Code-Exam.git
+"""
--- dl_exam/vlm/clip_latest/transformer.py
+++ dl_exam/vlm/clip_latest/transformer.py
@@ -11,7 +11,7 @@ from torch.nn import functional as F
 from torch.utils.checkpoint import checkpoint
 from collections import OrderedDict
 from typing import Callable, Dict, List, Optional
-from typing import Sequence, Tuple, Type, Union
+from typing import Sequence, Tuple, Type, Union, Literal
 from .utils import to_2tuple, feature_take_indices
 from .pos_embed import get_2d_sincos_pos_embed
 
@@ -74,6 +74,54 @@ class LayerNorm(nn.LayerNorm):
         x = F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
         return x.to(orig_type)
     
+
+"""
+class GELU(nn.Module):
+    def __init__(self, approximate: Literal['none', None]=None):
+        super().__init__()
+        if approximate not in ('none', None):
+            raise NotImplementedError("目前仅实现了精确的GELU(approximate=None)")
+        self.approximate = approximate
+    def forward(self, x:torch.Tensor)->torch.Tensor:
+        # GELU原始公式为 GELU(x)=0.5·x·(1 + erf(x/sqrt(2)))
+        if self.approximate is None:
+            return x * 0.5 * (1 + torch.erf(x / torch.sqrt(torch.tensor(2.0, dtype=x.dtype, device=x.device))))
+        else:
+            return x * torch.sigmoid(1.702 * x)
+"""  
+class QuickGELU(nn.Module):
+    """GELU激活函数的快速近似实现
+    比官方nn.GELU/nn.SiLU慢,占显存更多,但实现更简洁"""
+    def forward(self, x:torch.Tensor)->torch.Tensor:
+        return x * torch.sigmoid(1.702 * x)
     
+
 class LayerScale(nn.Module):
-    
+    """LayerScale层缩放模块,常用于Transformer类模型中LLaMA,PaLM
+    核心作用是对特征层进行逐维度的可学习缩放,提升模型训练稳定性和表达能力
+    init_values: 缩放因子,默认为1e-5; inplace: 是否使用原地操作,默认为False"""
+    def __init__(self, dim:int, 
+                 init_values:float=1e-5, 
+                 inplace:bool=False):
+        super().__init__()
+        self.inplace = inplace
+        self.gamma = nn.Parameter(init_values * torch.ones(dim))
+    def forward(self, x:torch.Tensor)-> torch.Tensor:
+        return x.mul_(self.gamma) if self.inplace else x * self.gamma
+
+
+class PatchDropout(nn.Module):
+    """ PatchDropout机制的PyTorch模块:https://arxiv.org/abs/2212.00794
+    对ViT中的图像patch tokens进行随机丢弃,以增强模型泛化能力,属于正则化技术"""
+    def __init__(self, prob:float=0.5,
+                 exclude_first_token:bool=True)->None:
+        assert 0<=prob<1, f'prob:{prob}必须在0到1之间'
+        self.prob = prob
+        self.exclude_first_token = exclude_first_token
+    def forward(self, x:torch.Tensor)->torch.Tensor:
+        if not self.training or self.prob==0: return x
+        # 判断是否包括first_token
+        if self.exclude_first_token: 
+            cls_tokens, x = x[:, :1], x[:, 1:]
+        else: cls_tokens = torch.jit.annotate(torch.Tensor, x[:, :1])
+        
