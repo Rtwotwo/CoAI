3ebce499f2e203bc392fe7e6f001dfff8ad04c8b
Rtwotwo <yoloredal@gmail.com>
2026-01-17 23:11:39 +0800

    Update loss.py

--- dl_exam/vlm/clip_latest/loss.py
+++ dl_exam/vlm/clip_latest/loss.py
@@ -90,11 +90,73 @@ class ClipLoss(nn.Module):
         # 创建缓存相关参数
         self.prev_num_logits = 0
         self.labels = {}
-    def get_ground_truth(self,):
+    def get_ground_truth(self, device, 
+                         num_logits
+                         )->torch.Tensor:
+        """生成/缓存并返回指定设备、指定长度的标签张量,适配分布式训练场景"""
+        # 触发重新生成标签的条件:本次需要的num_logits标签长度和上一次缓存的prev_num_logits不一致
+        if self.prev_num_logits != num_logits or device not in self.labels:
+            labels = torch.arange(num_logits, device=device, dtype=torch.long)
+            # 分布式训练的总进程数大于1且使用本地损失时,为不同进程生成不重叠的标签,避免标签冲突
+            if self.world_size > 1 and self.local_loss:
+                labels = labels + self.rank * num_logits
+            if self.cache_labels:
+                self.labels[device] = labels
+                self.prev_num_logits = num_logits
+        else:
+            labels = self.labels[device]
+        return labels
+    def get_logits(self, image_features, 
+                   text_features, 
+                   logit_scale, 
+                   logit_bias=None):
+        """计算图像-文本匹配对数概率:输入图像特征image_features,文本特征text_features,
+        结合温度系数logit_scale和可选偏置logit_bias,计算两组匹配得分"""
+        if self.world_size > 1:
+            # 在分布式场景下搜集所有进程和GPU的相关特征图
+            all_image_features, all_text_features = gather_features(
+                        image_features,
+                        text_features,
+                        local_loss=self.local_loss,
+                        gather_with_grad=self.gather_with_grad,
+                        rank=self.rank,
+                        world_size=self.world_size,
+                        use_horovod=self.use_horovod,)
+            # 仅用当前卡特征和全局特征计算,而非全局特征互乘
+            if self.local_loss:
+                logits_per_image = logit_scale * image_features @ all_text_features.T
+                logits_per_text = logit_scale * text_features @ all_image_features.T
+            else:
+                logits_per_image = logit_scale * all_image_features @ all_text_features.T
+                logits_per_text = logits_per_image.T
+        else:
+            logits_per_image = logit_scale * image_features @ text_features.T
+            logits_per_text = logit_scale * text_features @ image_features.T
+        # 添加可选偏置logit_bias参数进行求解
+        if logit_bias is not None:
+            logits_per_image += logit_bias
+            logits_per_text -= logit_bias
+        return logits_per_image, logits_per_text
+    def forward(self, image_features,
+                text_features,
+                logit_scale,
+                logit_bias=None,
+                output_dict=False):
+        """对比学习Contrastive Learning中图文匹配任务的前向传播逻辑,核心目标是
+        计算图文之间的对比损失Contrastive Loss,让匹配的图文特征更相似,不匹配的更疏远"""
+        device = image_features.device
+        logits_per_image, logits_per_text = self.get_logits(image_features, 
+                                                            text_features,
+                                                            logit_scale, 
+                                                            logit_bias=logit_bias)
+        labels = self.get_ground_truth(device, logits_per_image.shape[0])
+        total_loss = (F.cross_entropy(logits_per_image, labels) + F.cross_entropy(logits_per_text, labels)) / 2
+        return {'contrastive_loss': total_loss} if output_dict else total_loss
+    
+
+class CoCaLoss(ClipLoss):
+    def __init__(self, ):
+        """"""
         
-    def get_logits(self, ):
-        """"""
-    def forward(self, ):
-        """"""
         
     
