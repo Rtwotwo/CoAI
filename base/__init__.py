3bfde6231d08e3eaf9914fcfdb91b1f836d02777
Rtwotwo <yoloredal@gmail.com>
2026-02-15 23:24:12 +0800

    rebuild work folders

---
+++ base/__init__.py
@@ -0,0 +1,8 @@
+"""
+Author: Redal
+Date: 2025-10-11
+Todo: __init__.py
+Homepage: https://github.com/Rtwotwo/Code-Exam.git
+"""
+import torch
+import torch.nn as nn
--- dl_exam/nlp/bert/bert.py
+++
@@ -1,6 +0,0 @@
-"""
-Author: Redal
-Date: 2025-10-11
-Todo: 
-Homepape: https://github.com/Rtwotwo/Code-Exam.git
-"""
